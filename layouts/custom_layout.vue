9c4853c32a8f11356732b1cf37fe32186b7b89fe
hiramatch <hiramatch@gmail.com>
2020-08-16 20:42:40 +0900

    Add demo layout and page

---
+++ layouts/custom_layout.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>This is Custom Layout</h1>
+    <nuxt />
+  </div>
+</template>
---
+++ pages/demo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2 class="green">
+      This is Demo Page
+    </h2>
+    <p>Hello {{ name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'custom_layout',
+  asyncData (context) {
+    return { name: 'World' }
+  }
+}
+</script>
+<style>
+  .green {
+    color: green;
+  }
+</style>
